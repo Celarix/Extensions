eb51efb2f9c15e28bd9d2c7281deaa25cd0a6248
Chris Akridge <cakridge2@gmail.com>
2014-02-27 22:12:51 -0500

    Added project and DateTime date equals method
    Also unit testing is awesome

---
+++ Extensions/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool DatesEqual(this DateTime a, DateTime b)
+        {
+            return (a.Year == b.Year) &&
+                   (a.Month == b.Month) &&
+                   (a.Day == b.Day);
+        }
+    }
+}
---
+++ Extensions/ExtensionsTesting/DateTimeExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extensions;
+
+namespace ExtensionsTesting
+{
+    [TestClass]
+    public class DateTimeExtensionsTest
+    {
+        [TestMethod]
+        public void DatesEqualTest()
+        {
+            DateTime a = new DateTime(2014, 2, 27);
+            DateTime b = new DateTime(2014, 2, 27);
+
+            Assert.IsTrue(a.DatesEqual(b));
+        }
+
+        [TestMethod]
+        public void DatesUnequalTest()
+        {
+            DateTime a = new DateTime(2014, 2, 27);
+            DateTime b = new DateTime(2014, 2, 28);
+
+            Assert.IsFalse(a.DatesEqual(b));
+        }
+    }
+}
